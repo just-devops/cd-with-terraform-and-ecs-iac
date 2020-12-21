b8bf3e1ea3314fbeed8c49cb5e897d60e8092f0c
paschalidi <paschalidi@outlook.com>
2020-12-21 11:18:15 +0100

    chore: creates first module

--- main/main.tf
+++ main/main.tf
@@ -1,9 +1,22 @@
 terraform {
-  required_version = ">=0.13"
+  required_version = ">=0.12"
   backend "s3" {
     region  = "us-east-1"
     profile = "aws-devops"
-    key     = "tf-ecs-continuous-deployment"
-    bucket  = "tf-ecs-continuous-deployment"
+    key     = "tf-fargate-ecs-continuous-deployment"
+    bucket  = "tf-fargate-ecs-continuous-deployment"
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "codepipeline" {
+  source              = "./modules/codepipeline"
+  aws_region          = var.aws_region
+  profile             = var.profile
+  repo_owner          = var.repo_owner
+  github_access_token = var.github_access_token
+  repo_name           = var.repo_name
 }
